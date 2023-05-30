a21d6551f7dd57f59e561c86dda4fd273bbf8d7f
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-05-30 20:50:38 +0530

    Update benz5116.sql

--- sqlproject/Database2/dbo/Tables/benz5116.sql
+++ sqlproject/Database2/dbo/Tables/benz5116.sql
@@ -1,8 +1,8 @@
 /**comment**/
 CREATE TABLE [dbo].[benz5116] (
     [benzId]   INT           NULL,
-    /**This is for testing**/
     [benzname] NVARCHAR (30) NULL,
-    [benztag] NVARCHAR (30) NULL
+    [benztag] NVARCHAR (30) NULL,
+    [benzcg] NVARCHAR (30) NULL,
 );
 
