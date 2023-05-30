0be1ed2faf8324dc8773fa6880672cf10cf549ba
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-05-30 21:26:00 +0530

    Update benz5116.sql

--- sqlproject/Database2/dbo/Tables/benz5116.sql
+++ sqlproject/Database2/dbo/Tables/benz5116.sql
@@ -1,9 +1,10 @@
 /**comment**/
-CREATE TABLE [dbo].[benz5116] (
+/**CREATE TABLE [dbo].[benz5116] (
     [benzId]   INT           NULL,
     [benzname] NVARCHAR (30) NULL,
     [benztag] NVARCHAR (30) NULL,
     [benzcg] NVARCHAR (30) NULL,
     [benzcg2345] NVARCHAR (30) NULL,
-);
+);**/
+insert into benz5116 values(1,'abc','def','ghi','jkl');
 
